482d2a51201d91b182907a8b1f47aa504aac4ee9
Kasper L. Ekelund <kasperekelund@gmail.com>
2020-07-24 13:45:36 +0200

    Updates

---
+++ precache-manifest.1a45a25dc0ae365cff6a22bfb7115f89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e610458fc4603c7120f6a7b5b844cfe",
+    "url": "/react-chief-editor/index.html"
+  },
+  {
+    "revision": "19fd6f072b333bef1ae1",
+    "url": "/react-chief-editor/static/css/main.6ff5b65c.chunk.css"
+  },
+  {
+    "revision": "403d90ab07ff4e64e1f9",
+    "url": "/react-chief-editor/static/js/2.71cff098.chunk.js"
+  },
+  {
+    "revision": "5c1615e7d4ba5641752a11ee63eef588",
+    "url": "/react-chief-editor/static/js/2.71cff098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19fd6f072b333bef1ae1",
+    "url": "/react-chief-editor/static/js/main.2ce8bec4.chunk.js"
+  },
+  {
+    "revision": "690fc74d29616e3bd386",
+    "url": "/react-chief-editor/static/js/runtime-main.0838db46.js"
+  }
+]);
